e236fb70d256b378e5b48273995dd72854b58ade
@p_craft <ef81sp@gmail.com>
2020-04-21 00:34:48 +0900

    サイコロの個数を指定できるようにした

--- src/components/RollDice.js
+++ src/components/RollDice.js
@@ -1,33 +1,63 @@
 import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Slider from '@material-ui/core/Slider';
+import { makeStyles } from '@material-ui/core/styles';
 
 import Die6 from './RollDiceDie6';
 
+const useStyles = makeStyles({
+  slider: {
+    marginTop: '3em',
+  },
+});
+
 function RollDice() {
-  const [num, setNum] = useState(1);
+  const classes = useStyles();
+  const [nums, setNums] = useState([1]);
+  const [diceLength, setDiceLength] = useState(3);
+
+  const rollingDice = (diceLength, max) => {
+    const dice = [];
+    dice.length = diceLength;
+    dice.fill(1);
+    const id = setInterval(() => {
+      setNums(dice.map(() => getRandomInt(max)));
+    }, 400);
+    setTimeout(() => clearInterval(id), 1600);
+  };
+
   return (
     <div>
-      <div>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => {
-            const id = setInterval(() => {
-              setNum(getRandomInt6());
-            }, 70);
-            setTimeout(() => clearInterval(id), 260);
-          }}
-        >
-          1D6
-        </Button>
-      </div>
-      <Die6 num={num} />
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => rollingDice(diceLength, 6)}
+      >
+        {diceLength}D6
+      </Button>
+
+      <Slider
+        className={classes.slider}
+        defaultValue={3}
+        value={diceLength}
+        onChange={(e, v) => setDiceLength(v)}
+        step={1}
+        marks
+        min={1}
+        max={10}
+        valueLabelDisplay="on"
+      />
+
+      <Grid container spacing={2}>
+        {nums.map((n, i) => (
+          <Grid item key={i}>
+            <Die6 num={n} key={i} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
-}
-
-function getRandomInt6() {
-  return getRandomInt(6);
 }
 
 function getRandomInt(max) {
